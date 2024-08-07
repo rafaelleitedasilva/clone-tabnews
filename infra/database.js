8765d3db29ec9feb7aa470f245d2a32611825c2b
Rafael <69431917+rafaelleitedasilva@users.noreply.github.com>
2024-08-07 07:49:24 -0300

    Refactor DatabaseJS: Adjusting database.js to be a manipulator of the database

--- infra/database.js
+++ infra/database.js
@@ -1,96 +1,46 @@
 import { Client } from "pg";
 
-const config = {
-  database: {
-    max_connections: "",
-    opened_connections: "",
-    version: "",
-  },
-};
+async function query(queryObject) {
+  let client;
+  try {
+    client = await getNewClient();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await client.end();
+  }
+}
 
-function pgclient() {
-  return new Client({
+async function getNewClient() {
+  const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    connectionTimeoutMillis: 18000,
+    ssl: getSSLValues(),
   });
+
+  await client.connect();
+  return client;
 }
 
-async function query(queryString) {
-  const client = pgclient();
-
-  try {
-    await client.connect();
-    const result = await client.query(queryString);
-    return result;
-  } catch (error) {
-    throw error;
-  } finally {
-    await client.end();
-  }
-}
-
-async function version() {
-  const client = pgclient();
-
-  try {
-    await client.connect();
-    const select = await client.query(`SHOW server_version;`);
-    const version = parseFloat(select.rows[0].server_version);
-    config.database.version = version;
-  } catch (error) {
-    throw error;
-  } finally {
-    await client.end();
-  }
-}
-
-async function max_connections() {
-  const client = pgclient();
-
-  try {
-    await client.connect();
-    const select = await client.query(`SHOW max_connections;`);
-    const max_connections = parseInt(select.rows[0].max_connections);
-
-    config.database.max_connections = max_connections;
-  } catch (error) {
-    throw error;
-  } finally {
-    await client.end();
-  }
-}
-
-async function opened_connections() {
-  const client = pgclient();
-  const databasename = process.env.POSTGRES_DB;
-
-  try {
-    await client.connect();
-    const select = await client.query({
-      text: `SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;`,
-      values: [databasename],
-    });
-    const opened_connections = select["rows"][0]["count"];
-    config.database.opened_connections = opened_connections;
-  } catch (error) {
-    throw error;
-  } finally {
-    await client.end();
-  }
-}
-
-async function status() {
-  await version();
-  await max_connections();
-  await opened_connections();
-  return config;
-}
-
-export default {
-  query: query,
-  status: status,
+const database = {
+  query,
+  getNewClient,
 };
+
+export default database;
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "production" ? true : false;
+}
